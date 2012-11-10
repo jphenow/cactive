42f4da9eeba8f8ff3afd3af7c0fb383c8cc1fe36
Jon Phenow <j.phenow@gmail.com>
2012-11-10 12:23:54 -0600

    String access #from and #to

--- ext/string/access.c
+++ ext/string/access.c
@@ -1,8 +1,6 @@
 // Include the Ruby headers and goodies
 #include "ruby.h"
 
-// Prototype for our method 'test1' - methods are prefixed by 'method_' here
-//VALUE method_from(VALUE self, VALUE position);
 /*VALUE method_at(VALUE self, VALUE position);*/
 VALUE method_from(VALUE self, VALUE position);
 VALUE method_to(VALUE self, VALUE position);
@@ -18,75 +16,12 @@ void Init_string() {
   /*rb_define_method(rb_cString, "last", method_last, 0);*/
 }
 
-static VALUE
-rb_str_aref(VALUE str, VALUE indx)
-{
-    long idx;
-
-    if (FIXNUM_P(indx)) {
-	idx = FIX2LONG(indx);
-
-      num_index:
-	str = rb_str_substr(str, idx, 1);
-	if (!NIL_P(str) && RSTRING_LEN(str) == 0) return Qnil;
-	return str;
-    }
-
-    if (SPECIAL_CONST_P(indx)) goto generic;
-    switch (BUILTIN_TYPE(indx)) {
-      case T_REGEXP:
-	return rb_str_subpat(str, indx, INT2FIX(0));
-
-      case T_STRING:
-	if (rb_str_index(str, indx, 0) != -1)
-	    return rb_str_dup(indx);
-	return Qnil;
-
-      generic:
-      default:
-	/* check if indx is Range */
-	{
-	    long beg, len;
-	    VALUE tmp;
-
-	    len = str_strlen(str, STR_ENC_GET(str));
-	    switch (rb_range_beg_len(indx, &beg, &len, len, 0)) {
-	      case Qfalse:
-		break;
-	      case Qnil:
-		return Qnil;
-	      default:
-		tmp = rb_str_substr(str, beg, len);
-		return tmp;
-	    }
-	}
-	idx = NUM2LONG(indx);
-	goto num_index;
-    }
-
-    UNREACHABLE;
-}
-
 VALUE method_from(VALUE self, VALUE position) {
-  end = RSTRING_LEN(self);
-  start = FIX2INT(position);
-  length = end - start;
-  VALUE new_string[length];
-  for(i = start; i < end + 1; i++)
-  {
-    new_string[i-start] = rb_str_aref(i, self);
-  }
-  newstr = rb_str_new(0, length);
-  for(i = 0; i < sizeof(new_string) + 1; i++)
-  {
-    newstr = rb_string_concat(newstr, new_string[i]);
-  }
-  return newstr;
+  return rb_str_substr(self, FIX2INT(position), RSTRING_LEN(self) - 1);
 }
 
 VALUE method_to(VALUE self, VALUE position) {
-  VALUE positions[2] = { INT2FIX(0), position };
-  return rb_str_aref(2, positions, self);
+  return rb_str_substr(self, 0, (FIX2INT(position) + 1));
 }
 
 /*VALUE method_second(VALUE self) {*/
