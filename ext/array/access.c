d2e59d753866ab1d8ce3fe1c8f92d6ad5355967c
Jon Phenow <j.phenow@gmail.com>
2012-11-10 12:43:03 -0600

    just needed ruby/ruby.h so I didn't have to reimplement 'rb_ary_aref'

--- ext/array/access.c
+++ ext/array/access.c
@@ -1,5 +1,6 @@
 // Include the Ruby headers and goodies
 #include "ruby.h"
+#include "ruby/ruby.h"
 
 VALUE method_from(VALUE self, VALUE position);
 VALUE method_to(VALUE self, VALUE position);
@@ -18,39 +19,6 @@ void Init_array() {
   rb_define_method(rb_cArray, "fourth", method_fourth, 0);
   rb_define_method(rb_cArray, "fifth", method_fifth, 0);
   rb_define_method(rb_cArray, "forty_two", method_forty_two, 0);
-}
-
-// TODO Yes re-impleneted from array.c .... would love a better way plx thx
-VALUE rb_ary_aref(int argc, VALUE *argv, VALUE ary) {
-  VALUE arg;
-  long beg, len;
-
-  if (argc == 2) {
-    beg = NUM2LONG(argv[0]);
-    len = NUM2LONG(argv[1]);
-    if (beg < 0) {
-      beg += RARRAY_LEN(ary);
-    }
-    return rb_ary_subseq(ary, beg, len);
-  }
-  if (argc != 1) {
-    rb_scan_args(argc, argv, "11", 0, 0);
-  }
-  arg = argv[0];
-  /* special case - speeding up */
-  if (FIXNUM_P(arg)) {
-    return rb_ary_entry(ary, FIX2LONG(arg));
-  }
-  /* check if idx is Range */
-  switch (rb_range_beg_len(arg, &beg, &len, RARRAY_LEN(ary), 0)) {
-    case Qfalse:
-      break;
-    case Qnil:
-      return Qnil;
-    default:
-      return rb_ary_subseq(ary, beg, len);
-  }
-  return rb_ary_entry(ary, NUM2LONG(arg));
 }
 
 VALUE method_from(VALUE self, VALUE position) {
